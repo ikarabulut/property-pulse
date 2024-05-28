7f6b5a3dc43eb9a9b1d3833f5fd2115d55c1d385
Ismail Karabulut <i.karabulut93@gmail.com>
2024-05-28 16:26:09 -0500

    add hardcoded property info for property page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
-import Link from "next/link";
+import PropertyCard from "@/components/PropertyCard";
+
+import properties from "@/properties.json";
 
 const PropertiesPage = () => {
   return (
-    <>
-      <h1 className="text-3xl">Welcome</h1>
-      <Link href="/">Go Home</Link>
-    </>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No Properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
