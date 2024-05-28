55ae2a2f5f3698a4c3e2b20e1c648a44744b9328
Ismail Karabulut <i.karabulut93@gmail.com>
2024-05-28 08:40:20 -0500

    Init

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@/assets/styles/globals.css";
+
+import React from "react";
+
+export const metadata = {
+  title: "PropertyPulse | Find the Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
