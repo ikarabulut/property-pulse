0588588266ccb8a854adfd4deb4d579d15a38664
Ismail Karabulut <i.karabulut93@gmail.com>
2024-05-29 15:08:53 -0500

    WIP add fetch property

--- utils/requests.js
+++ utils/requests.js
@@ -20,4 +20,24 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+async function fetchProperty(id) {
+  try {
+    // handle case when domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
