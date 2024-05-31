bd138233abdc19be771f2762c6a94438386fcd43
Ismail Karabulut <i.karabulut93@gmail.com>
2024-05-31 13:27:13 -0500

    add Property feature implemented minus image upload

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -2,6 +2,8 @@ import connectDB from "@/config/database";
 
 import Property from "@/models/Property";
 
+import { getSessionUser } from "@/utils/getSessionUser";
+
 // GET /api/properties
 export const GET = async (request) => {
   try {
@@ -11,6 +13,64 @@ export const GET = async (request) => {
     return new Response(JSON.stringify(properties), {
       status: 200,
     });
+  } catch (error) {
+    console.log(error);
+    return new Response("Error", { status: 500 });
+  }
+};
+
+export const POST = async (request) => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User Id is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    const formData = await request.formData();
+    const amenities = formData.getAll("amenities");
+    const images = formData
+      .getAll("images")
+      .filter((image) => image.name !== "");
+
+    const propertyData = {
+      type: formData.get("type"),
+      name: formData.get("name"),
+      description: formData.get("description"),
+      location: {
+        street: formData.get("location.street"),
+        city: formData.get("location.city"),
+        state: formData.get("location.state"),
+        zipcode: formData.get("location.zipcode"),
+      },
+      beds: formData.get("beds"),
+      baths: formData.get("baths"),
+      square_feet: formData.get("square_feet"),
+      amenities: amenities,
+      rates: {
+        weekly: formData.get("rates.weekly"),
+        monthly: formData.get("rates.monthly"),
+        nightly: formData.get("rates.nightly"),
+      },
+      seller_info: {
+        name: formData.get("seller_info.name"),
+        email: formData.get("seller_info.email"),
+        phone: formData.get("seller_info.phone"),
+      },
+      owner: userId,
+      // images: images,
+    };
+
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    );
+    // return new Response(Json.stringify({message: "property added"}), 201);
   } catch (error) {
     console.log(error);
     return new Response("Error", { status: 500 });
--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -93,7 +93,11 @@ const PropertyAddForm = () => {
 
   return (
     mounted && (
-      <form>
+      <form
+        action="/api/properties"
+        method="POST"
+        encType="multipart/form-data"
+      >
         <h2 className="text-3xl text-center font-semibold mb-6">
           Add Property
         </h2>
@@ -555,6 +559,7 @@ const PropertyAddForm = () => {
             accept="image/*"
             multiple
             onChange={handleImageChange}
+            required
           />
         </div>
 
