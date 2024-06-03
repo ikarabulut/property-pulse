fa8cfce053268869fead3c366ba790e0eec4c8af
Ismail Karabulut <i.karabulut93@gmail.com>
2024-06-03 12:36:30 -0500

    add saved properties page

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -20,21 +20,17 @@ export const POST = async (request) => {
 
     const { userId } = sessionUser;
 
-    // Find user in database
     const user = await User.findOne({ _id: userId });
 
-    // Check if property is bookmarked
     let isBookmarked = user.bookmarks.includes(propertyId);
 
     let message;
 
     if (isBookmarked) {
-      // If already bookmarked, remove it
       user.bookmarks.pull(propertyId);
       message = "Bookmark removed successfully";
       isBookmarked = false;
     } else {
-      // If not bookmarked, add it
       user.bookmarks.push(propertyId);
       message = "Bookmark added successfully";
       isBookmarked = true;
@@ -45,6 +41,28 @@ export const POST = async (request) => {
     return new Response(JSON.stringify({ message, isBookmarked }), {
       status: 200,
     });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
+
+export const GET = async () => {
+  try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+    const user = await User.findOne({ _id: userId });
+
+    const bookmarks = await Property.find({ _id: { $in: user.bookmarks } });
+
+    return new Response(JSON.stringify(bookmarks), { status: 200 });
   } catch (error) {
     console.log(error);
     return new Response("Something went wrong", { status: 500 });
