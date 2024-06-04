56239e1cdd24a36d00b4fa9e683dcd39ee610ca6
Ismail Karabulut <i.karabulut93@gmail.com>
2024-06-04 13:49:49 -0500

    extract properties section to own component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-import PropertyCard from "@/components/PropertyCard";
-import { fetchProperties } from "@/utils/requests";
+import Properties from "@/components/Properties";
 import PropertySearchForm from "@/components/PropertySearchForm";
 
 const PropertiesPage = async () => {
-  const properties = await fetchProperties();
   return (
     <>
       <section className="bg-blue-700 py-4">
@@ -13,19 +11,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />
         </div>
       </section>
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No Properties found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>
-      </section>
+      <Properties />
     </>
   );
 };
