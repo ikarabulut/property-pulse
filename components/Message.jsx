946a746d94847c14b63460826888824f4a6df6a0
Ismail Karabulut <i.karabulut93@gmail.com>
2024-06-03 21:35:01 -0500

    add message read functionality

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,10 +1,61 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 const Message = ({ message }) => {
+  const [isRead, setIsRead] = useState(message.read);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const handleReadClick = async () => {
+    try {
+      const res = await fetch(`/api/messages/${message._id}`, {
+        method: "PUT",
+      });
+
+      if (res.status === 200) {
+        const { read } = await res.json();
+        setIsRead(read);
+        if (read) {
+          toast.success("Marked as read");
+        } else {
+          toast.success("Marked as new");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  const handleDeleteClick = async () => {
+    try {
+      const res = await fetch(`/api/messages/${message._id}`, {
+        method: "DELETE",
+      });
+
+      if (res.status === 200) {
+        setIsDeleted(true);
+        toast.success("Message Deleted");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Message was not deleted");
+    }
+  };
+
+  if (isDeleted) {
+    return null;
+  }
+
   return (
     <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
+      {!isRead && (
+        <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md">
+          New
+        </div>
+      )}
       <h2 className="text-xl mb-4">
-        <span className="font-bold">Property Inquiry: </span>
+        <span className="font-bold">Property Inquiry:</span>{" "}
         {message.property.name}
       </h2>
       <p className="text-gray-700">{message.body}</p>
@@ -15,13 +66,13 @@ const Message = ({ message }) => {
         </li>
 
         <li>
-          <strong>Reply Email:</strong>
+          <strong>Reply Email:</strong>{" "}
           <a href={`mailto:${message.email}`} className="text-blue-500">
             {message.email}
           </a>
         </li>
         <li>
-          <strong>Reply Phone:</strong>
+          <strong>Reply Phone:</strong>{" "}
           <a href={`tel:${message.phone}`} className="text-blue-500">
             {message.phone}
           </a>
@@ -31,14 +82,21 @@ const Message = ({ message }) => {
           {new Date(message.createdAt).toLocaleString()}
         </li>
       </ul>
-      <button className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md">
-        Mark As Read
+      <button
+        onClick={handleReadClick}
+        className={`mt-4 mr-3 ${
+          isRead ? "bg-gray-300" : "bg-blue-500 text-white"
+        } py-1 px-3 rounded-md`}
+      >
+        {isRead ? "Mark As New" : "Mark As Read"}
       </button>
-      <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
+      <button
+        onClick={handleDeleteClick}
+        className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md"
+      >
         Delete
       </button>
     </div>
   );
 };
-
 export default Message;
