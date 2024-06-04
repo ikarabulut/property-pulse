f4294f8ac499d1b876377dc1b37e21b3d7f9812b
Ismail Karabulut <i.karabulut93@gmail.com>
2024-06-04 14:27:45 -0500

    add lightbox component for photos

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,10 @@
 import "@/assets/styles/globals.css";
+import "photoswipe/dist/photoswipe.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import React from "react";
 
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
