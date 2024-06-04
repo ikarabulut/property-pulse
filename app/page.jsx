b5e387d9297a04d038f273dfe8798f3433a74c38
Ismail Karabulut <i.karabulut93@gmail.com>
2024-06-04 15:08:46 -0500

    add featured properties

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
+import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
       <InfoBoxes />
+      <FeaturedProperties />
       <HomeProperties />
     </div>
   );
--- utils/requests.js
+++ utils/requests.js
@@ -1,16 +1,19 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
-async function fetchProperties() {
+async function fetchProperties({ showFeatured = false } = {}) {
   try {
     // handle case when domain is not available yet
     if (!apiDomain) {
       return [];
     }
 
-    const res = await fetch(`${apiDomain}/properties`, { cache: "no-store" });
+    const res = await fetch(
+      `${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+      { cache: 'no-store' }
+    );
 
     if (!res.ok) {
-      throw new Error("Failed to fetch data");
+      throw new Error('Failed to fetch data');
     }
 
     return res.json();
